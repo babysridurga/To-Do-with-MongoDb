51edbef3c46a1939a92c711dc021af47335e7e0f
babysridurga <160447803+babysridurga@users.noreply.github.com>
2024-10-07 21:58:07 +0530

    server.js

--- todo/todo-backend/server.js
+++ todo/todo-backend/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = 'mongodb+srv://trail:12345@cluster0.2o698.mongodb.net/';
+const MONGODB_URI = 'Place Your Connection String Here';
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
